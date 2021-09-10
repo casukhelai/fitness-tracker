525ab1266301df8116a0a2bd713842910ede7c69
Ishya Casukhela <casukhelai@gmail.com>
2021-09-09 20:37:38 -0400

    created api routes for the workout pages

--- routes/api.js
+++ routes/api.js
@@ -6,6 +6,28 @@ const db = require("../models/workout.js");
 // view combined weight of multiple exercises from past 7 workouts on stats page
 // view total duration of each workout from the past 7 workouts on the stats page
 
+// getting all workouts
+router.get("/api/workouts", (req, res) => {
+    db.find()
+        .then((dbData) => {
+            res.json(dbData);
+        })
+        .catch((err) => {
+            res.json(err);
+        });
+});
 
+//
+router.get("/api/workouts/range", (req,res) => {
+    db.find()
+        .then((dbData) => {
+            res.json(dbData);
+        })  
+        .catch((err) => {
+            res.json(err);
+        });
+});
+
+// POST request
 
 module.exports = router;
