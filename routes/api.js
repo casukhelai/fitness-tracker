ac35e83568fbd5b070fbee3c19cf793d6790ca27
Ishya Casukhela <casukhelai@gmail.com>
2021-09-09 20:02:47 -0400

    started api routes

---
+++ routes/api.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+const db = require("../models/workout.js");
+
+// add exercises to most recent workout plan
+// add new exercises to a NEW workout plan
+// view combined weight of multiple exercises from past 7 workouts on stats page
+// view total duration of each workout from the past 7 workouts on the stats page
+
+
+
+module.exports = router;
